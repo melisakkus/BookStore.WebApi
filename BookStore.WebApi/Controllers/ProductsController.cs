dd5ed8c07ed1001c470e7682fb253114dc2d6ed1
Melisa Akkuş <melisa.akkus01@gmail.com>
2025-02-16 23:54:14 +0300

    Updates after second lesson

---
+++ BookStore.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using BookStore.BusinessLayer.Abstract;
+using BookStore.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            return Ok(_productService.TGetAll());
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            _productService.TAdd(product);
+            return Ok("Ekleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            _productService.TDelete(id);
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _productService.TUpdate(product);
+            return Ok("Güncelleme işlemi başarılı");
+        }
+
+        [HttpGet("GetProduct")]
+        public IActionResult GetProduct(int id)
+        {
+            return Ok(_productService.TGetById(id));
+        }
+
+        [HttpGet("ProductCount")]
+        public IActionResult ProductCount()
+        {
+            return Ok(_productService.TGetProductCount());
+        }
+    }
+}

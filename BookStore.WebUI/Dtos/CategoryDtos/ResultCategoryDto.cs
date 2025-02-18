72724e411c3657e8678a0a1408c02c559f5660d4
Melisa Akkuş <melisa.akkus01@gmail.com>
2025-02-18 20:43:32 +0300

    updates for third lesson

---
+++ BookStore.WebUI/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.WebUI.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
+
+//apiden gelen veriyi ui tarafındaki model ile eşleştirmek için 
